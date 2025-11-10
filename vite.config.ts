9a0d6384eecdb2cdbe66fd02d75e272025b4e828
ariasmarcelo <ariasmarcelo@users.noreply.github.com>
2025-11-10 14:54:12 -0300

    chore: atualizar URLs para site-igreja-v6

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/site-igreja-v5/' : '/',
+  base: mode === 'production' ? '/site-igreja-v6/' : '/',
   server: {
     host: "::",
     port: 8080,
